a2717b10781f7f82172f7629391999eb4a8a0217
Doom² <Anosphiroth@gmail.com>
2026-06-23 21:58:24 +0200

    Final version (hopefully)

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vaamonch <vaamonch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 14:32:20 by zsonie            #+#    #+#             */
-/*   Updated: 2026/06/23 20:36:52 by vaamonch         ###   ########.fr       */
+/*   Updated: 2026/06/23 21:57:59 by vaamonch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,6 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-	// only for testing, remove this part
-	if (argc == 1) {
-		try {
-			Server server(4242, "pass");
-			server.update();
-		} catch (const std::exception& e) {
-			std::cerr << "Error: " << e.what() << std::endl;
-			return 1;
-		}
-		return 0;
-	}
-	// true project
 	if (argc != 3) {
 		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
 		return 1;
